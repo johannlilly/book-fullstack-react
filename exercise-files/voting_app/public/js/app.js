c6c6238954cf5994db9deffc580d7e91d0b16409
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2019-12-14 14:42:54 -0500

    Use props instead of hard-coding data

--- exercise-files/voting_app/public/js/app.js
+++ exercise-files/voting_app/public/js/app.js
@@ -22,14 +22,31 @@ class ProductList extends React.Component {
 class Product extends React.Component {
   render() {
     return (
-      <div className='item'> <div className='image'>
-        <img src='images/products/image-aqua.png' /> </div>
-        <div className='middle aligned content'> <div className='description'>
-          <a>Fort Knight</a>
-          <p>Authentic renaissance actors, delivered in just two weeks.</p> </div>
-          <div className='extra'> <span>Submitted by:</span> <img
-            className='ui avatar image'
-            src='images/avatars/daniel.jpg' />
+      <div className='item'>
+        <div className='image'>
+          <img src={this.props.productImageUrl} />
+        </div>
+        <div className='middle aligned content'>
+          <div className='header'>
+            <a>
+              <i className='large caret up icon' />
+            </a>
+            {this.props.votes}
+          </div>
+          <div className='description'>
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
+            <p>
+              {this.props.description}
+            </p>
+          </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+              className='ui avatar image'
+              src={this.props.submitterAvatarUrl}
+            />
           </div>
         </div>
       </div>
