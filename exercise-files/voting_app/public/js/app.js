a66a607cc5ae215342345215bc9009f9d7349b39
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2019-12-14 14:52:26 -0500

    Functionally render multilple child components using map

--- exercise-files/voting_app/public/js/app.js
+++ exercise-files/voting_app/public/js/app.js
@@ -1,19 +1,21 @@
 
 class ProductList extends React.Component {
   render() {
-    const product = Seed.products[0];
+    const productComponents = Seed.products.map((product) => (
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+      />
+    ));
     return (
       <div className='ui unstackable items'>
-        <Product
-          // [propName]=[propValue]
-          id={product.id}
-          title={product.title}
-          description={product.description}
-          url={product.url}
-          votes={product.votes}
-          submitterAvatarUrl={product.submitterAvatarUrl}
-          productImageUrl={product.productImageUrl}
-        />
+        {productComponents}
       </div>
     );
   }
