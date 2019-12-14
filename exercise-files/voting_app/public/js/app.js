fed01d7b702dbb7c155c9076800aa074c3158fd9
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2019-12-14 15:46:11 -0500

    Sort child components with Array's sort method

--- exercise-files/voting_app/public/js/app.js
+++ exercise-files/voting_app/public/js/app.js
@@ -1,7 +1,10 @@
 
 class ProductList extends React.Component {
   render() {
-    const productComponents = Seed.products.map((product) => (
+    const products = Seed.products.sort((a, b) => (
+      b.votes - a.votes
+    ));
+    const productComponents = products.map((product) => (
       <Product
         key={'product-' + product.id}
         id={product.id}
