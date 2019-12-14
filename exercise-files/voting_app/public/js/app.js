609b31a89533d545b02312cfa53ac13d6a044f75
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2019-12-14 14:38:32 -0500

    hard-code seed data

--- exercise-files/voting_app/public/js/app.js
+++ exercise-files/voting_app/public/js/app.js
@@ -1,9 +1,19 @@
 
 class ProductList extends React.Component {
   render() {
+    const product = Seed.products[0];
     return (
       <div className='ui unstackable items'>
-        <Product />
+        <Product
+          // [propName]=[propValue]
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          url={product.url}
+          votes={product.votes}
+          submitterAvatarUrl={product.submitterAvatarUrl}
+          productImageUrl={product.productImageUrl}
+        />
       </div>
     );
   }
