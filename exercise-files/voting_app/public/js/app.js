25cd9cb98a78aed92fd4608eb243075a241baf72
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2019-12-14 14:26:10 -0500

    Define child component

--- exercise-files/voting_app/public/js/app.js
+++ exercise-files/voting_app/public/js/app.js
@@ -1,18 +1,33 @@
+
 class ProductList extends React.Component {
-    render() {
-      return (
-        <div className='ui unstackable items'>
-          Hello, friend! I am a basic React component.
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, friend! I am a basic React component.
+            </div>
+    );
   }
+}
 
-// tell the React framework that our component should be inserted on this page.
-// instruct React to render this ProductList inside a specific DOM node
+class Product extends React.Component {
+  render() {
+    return (
+      <div className='item'> <div className='image'>
+        <img src='images/products/image-aqua.png' /> </div>
+        <div className='middle aligned content'> <div className='description'>
+          <a>Fort Knight</a>
+          <p>Authentic renaissance actors, delivered in just two weeks.</p> </div>
+          <div className='extra'> <span>Submitted by:</span> <img
+            className='ui avatar image'
+            src='images/avatars/daniel.jpg' />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-// ReactDOM.render([what], [where]);
 ReactDOM.render(
-    <ProductList />, // what to render
-    document.getElementById('content') // where to render it
+  <ProductList />, // what to render
+  document.getElementById('content') // where to render it
 );
