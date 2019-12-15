430216dc4a9d09639735bfda4662d789d7a56897
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2019-12-15 13:51:02 -0500

    Data-driven react with interactivity

--- exercise-files/voting_app/public/js/app.js
+++ exercise-files/voting_app/public/js/app.js
@@ -1,5 +1,10 @@
 
 class ProductList extends React.Component {
+
+  handleProductUpVote(productId) {
+    console.log(productId + ' was upvoted.');
+  }
+
   render() {
     const products = Seed.products.sort((a, b) => (
       b.votes - a.votes
@@ -14,6 +19,7 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
       />
     ));
     return (
@@ -25,6 +31,9 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
   render() {
     return (
       <div className='item'>
@@ -33,7 +42,7 @@ class Product extends React.Component {
         </div>
         <div className='middle aligned content'>
           <div className='header'>
-            <a>
+            <a onClick={this.handleUpVote}>
               <i className='large caret up icon' />
             </a>
             {this.props.votes}
